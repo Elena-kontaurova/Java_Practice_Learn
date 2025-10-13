61acffbe9680851f0129714f4aad73588c44a6a8
Elena-kontaurova <kontaurova0306@gmail.com>
2025-10-13 23:57:36 +0300

    ай

---
+++ src/theory/ExpansionContraction.java
@@ -0,0 +1,39 @@
+package theory;
+
+import java.io.FileNotFoundException;
+
+// Приведение примитивных типов: расширение и сужение
+public class ExpansionContraction {
+    public static void main(String[] args) {
+        numberThree();
+    }
+    public static void intType() {
+        int a = 0;
+        int b =  a + 46;
+        byte c = (byte) (a*b);
+        double f = (double) 1234.15;
+        long d = (long) (a + f / c + b);
+        System.out.println(d);;
+    }
+    public static void numberTwo() {
+        int a = 15;
+        int b = 4;
+        float c = (float) a / b;
+        double d = (double) a * 1e-3 + c;
+        System.out.println(d);;
+    }
+    public static void numberThree() {
+        float f = (float)128.50;
+        int i = (int)f;
+        int b = (int)(i + f);
+        byte d = (byte) b;
+        System.out.println(d);;
+    }
+    public static void numberFour() {
+        short number = 9;
+        char zero = '0';
+        int nine = (zero + number);
+        short v = (short) nine;
+        System.out.println(v);
+    }
+}
